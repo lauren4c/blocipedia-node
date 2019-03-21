e64d82c11d5edf5d8f979db9f000b0e15799ee02
Lauren Forcey <lauren4c@gmail.com>
2019-03-21 12:22:56 -0600

    Updated utilities/email.js file - switched send/recieve

--- src/utilities/email.js
+++ src/utilities/email.js
@@ -2,8 +2,8 @@ const sgMail = require("@sendgrid/mail");
 module.exports = function(username, email) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
-    to: "lauren4c@gmail.com",
-    from: { email: email, name: "Blocipedia" },
+    to: email,
+    from: { email: "lauren4c@gmail.com", name: "Blocipedia" },
     subject: "Test Sendgrid Mail",
     text: `Hi there, ${username}! Thanks for signing up for Blocipedia. Your account is ready for you.`,
     html: `<a href:"https://blocipedia-4c.herokuapp.com/">Blocipedia</a>`
